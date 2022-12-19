ddc281a35c319a19141d5db8f44f6844ca47511f
by_Onyx <ilyayaldinov@gmail.com>
2022-12-19 20:25:38 +0500

    Вкладки доработанно

--- src/main/java/org/example/TabbedPaneTest.java
+++ src/main/java/org/example/TabbedPaneTest.java
@@ -8,43 +8,33 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TabbedPaneTest extends JFrame
 {
-    private  final  Color[]  colors = {Color.cyan, Color.orange, Color.magenta, Color.blue,
-            Color.red , Color.green , Color.yellow , Color.pink };
-    private  final  String   TEMPL_label   = "Метка %d";
-    private  final  String   TEMPL_dynamic = "Динамическая метка %d";
-    private  final  String   TEMPL_button  = "Кнопка %d";
-    private  final  String   TEMPL_tab     = "Вкладка %d";
+    private List<String> tabNames = new ArrayList<>();
+    private List<JPanel> panels = new ArrayList<>();
     public TabbedPaneTest()
     {
-        super("Пример панели с вкладками JTabbedPane");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        // Левая панель с вкладками
         JTabbedPane tabsLeft = new JTabbedPane(JTabbedPane.TOP,
                 JTabbedPane.SCROLL_TAB_LAYOUT);
-        // Создание вкладок
-        for (int i = 1; i <= colors.length; i++) {
-            JPanel panel = new JPanel();
-            // Подкрашиваем панель
-            panel.setBackground(colors[i - 1]);
-            // Размещение метки во вкладке
-            panel.add(new JLabel(String.format(TEMPL_label, i)));
-            // Добавление вкладки
-            tabsLeft.addTab(String.format(TEMPL_tab, i), panel);
-            // Подключение мнемоники
-            tabsLeft.setMnemonicAt(i-1, String.valueOf(i).charAt(0));
+
+        setValues();
+
+        for(int i = 1; i <= panels.size(); i++){
+            tabsLeft.addTab(tabNames.get(i - 1), panels.get(i - 1));
+            tabsLeft.setMnemonicAt(i - 1, String.valueOf(i).charAt(0));
         }
         // Подключение слушателя событий
         tabsLeft.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 // Получение выделенной вкладки
                 JPanel panel = (JPanel)((JTabbedPane)e.getSource()).getSelectedComponent();
-                // Количество компонентов в панели
-                int count = panel.getComponentCount();
                 // Добавление на вкладку новой метки
-                panel.add(new JLabel(String.format(TEMPL_dynamic, count)));
+                panel.add(new JLabel("Haha"));
             }
         });
         // Подключение слушателя мыши
@@ -56,10 +46,26 @@ public class TabbedPaneTest extends JFrame
             }
         });
         getContentPane().add(tabsLeft);
-        setSize(600, 250);
+        setSize(1280, 720);
         setVisible(true);
     }
     public static void main(String[] args) {
         new TabbedPaneTest();
+    }
+
+    public JPanel getNewTabbedPane(String name){
+        JPanel panel = new JPanel();
+        panel.add(new JLabel(name));
+        return panel;
+    }
+
+    public void setValues(){
+        panels.add(getNewTabbedPane("Финансы"));
+        panels.add(getNewTabbedPane("Управление"));
+        panels.add(getNewTabbedPane("Настройки"));
+
+        tabNames.add("Финансы");
+        tabNames.add("Управление");
+        tabNames.add("Настройки");
     }
 }
